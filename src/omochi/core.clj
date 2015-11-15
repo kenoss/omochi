508478dc1998570a7c05cdb001fc0831be7f6e66
Ken OKADA <keno.ss57@gmail.com>
2015-11-16 07:25:36 +0900

    Force yamabiko command starts BOL

--- src/omochi/core.clj
+++ src/omochi/core.clj
@@ -56,7 +56,7 @@
 
 (defn yamabiko
   [{:keys [channel user text]}]
-  (when-let [text (last (re-find #"!yamabiko (.+)" text))]
+  (when-let [text (last (re-find #"^!yamabiko (.+)$" text))]
     (emit! :slacker.client/send-message channel
            (format "<@%s>: %s" user text))))
 
